46986a9dbe990b08d77f202666f9cd1536762fa7
rayanaOmar <rayanaomar_@hotmail.com>
2021-12-24 18:50:47 +0000

    MadLibs

---
+++ MadLibs/InfoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InfoViewController.swift
+//  MadLibs
+//
+//  Created by admin on 24/12/2021.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var adTextField: UITextField!
+    @IBOutlet weak var verbOneTF: UITextField!
+    @IBOutlet weak var verbTwoTF: UITextField!
+    @IBOutlet weak var nounTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fullStatmentLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind(_ unwindSegue: UIStoryboardSegue) {
+        // Use data from the view controller which initiated the unwind segue
+        
+        if let secondPage = unwindSegue.source as? InfoViewController{
+            if let ad = secondPage.adTextField.text,
+               let verbOne = secondPage.verbOneTF.text,
+               let verbTwo = secondPage.verbTwoTF.text,
+               let noun = secondPage.nounTF.text {
+                fullStatmentLabel.text = "We are having a perfcectly \(ad) time now. Later we will \(verbOne) and  \(verbTwo) in the \(noun)."
+            }
+        }
+        
+        
     }
 
 
